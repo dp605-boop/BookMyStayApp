13b5fb2e4cc41f343cb0a43d6f65f4999e671bee
beyofrin <beyofrin40@gmail.com>
2026-03-11 14:14:16 +0530

    [durga]added[feature/UC1-WelcomePage]

---
+++ APP/src/BookMyStayApp.java
@@ -0,0 +1,12 @@
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("      Book My Stay Application   ");
+        System.out.println("=================================");
+        System.out.println("Application started successfully.");
+        System.out.println("Welcome to the Hotel Booking System!");
+    }
+
+}
---
+++ APP/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,11 @@
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("   Hotel Booking Management App  ");
+        System.out.println("=================================");
+        System.out.println("Application started successfully.");
+        System.out.println("Welcome to the Hotel Booking System!");
+    }
+
+}
